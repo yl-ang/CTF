ae99c3da23c04a9c403f6b64aa09dfe609e7bd74
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2021-06-26 15:33:39 +0800

    commit

---
+++ CDDC21/RE/RE2.c
@@ -0,0 +1,54 @@
+lVar2 = std::__cxx11::basic_string<char,std::char_traits<char>,std::allocator<char>>::size();
+  if (lVar2 == 8) {
+    pcVar3 = (char *)std::__cxx11::basic_string<char,std::char_traits<char>,std::allocator<char>>::
+                     operator[]((ulong)local_78);
+    if (*pcVar3 == 'K') {
+      pcVar3 = (char *)std::__cxx11::basic_string<char,std::char_traits<char>,std::allocator<char>>
+                       ::operator[]((ulong)local_78);
+      if (*pcVar3 == 'Z') {
+        pcVar3 = (char *)std::__cxx11::
+                         basic_string<char,std::char_traits<char>,std::allocator<char>>::operator[]
+                                   ((ulong)local_78);
+        if (*pcVar3 == 'e') {
+          pcVar3 = (char *)std::__cxx11::
+                           basic_string<char,std::char_traits<char>,std::allocator<char>>::
+                           operator[]((ulong)local_78);
+          if (*pcVar3 == 'M') {
+            pcVar3 = (char *)std::__cxx11::
+                             basic_string<char,std::char_traits<char>,std::allocator<char>>::
+                             operator[]((ulong)local_78);
+            if ((int)*pcVar3 == local_7c + local_80) {
+              pcVar3 = (char *)std::__cxx11::
+                               basic_string<char,std::char_traits<char>,std::allocator<char>>::
+                               operator[]((ulong)local_78);
+              if (*pcVar3 == 'a') {
+                pcVar3 = (char *)std::__cxx11::
+                                 basic_string<char,std::char_traits<char>,std::allocator<char>>::
+                                 operator[]((ulong)local_78);
+                if (*pcVar3 == 'S') {
+                  pcVar3 = (char *)std::__cxx11::
+                                   basic_string<char,std::char_traits<char>,std::allocator<char>>::
+                                   operator[]((ulong)local_78);
+                  if (*pcVar3 == 'r') {
+                    bVar1 = true;
+                    goto LAB_00401098;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  bVar1 = false;
+}
+
+
+/*
+The password to the binary is KZeMZaSr, the key size is 8.
+Flag is CDDC21{k5JkMxP66d}
+*/
+
+
+
